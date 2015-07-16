7300d1c7da2cade93117cf3f874bf1d521e6e2fe
Diego Valdivia <dmarvp@hotmail.com>
2015-07-16 02:24:18 +0000

    Added a description to Recurrent Transactions

--- mobileproject/mobileapp/db/schema.rb
+++ mobileproject/mobileapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714034800) do
+ActiveRecord::Schema.define(version: 20150716021724) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -53,8 +53,9 @@ ActiveRecord::Schema.define(version: 20150714034800) do
     t.integer  "period_id",   limit: 4
     t.date     "nextdate"
     t.integer  "customer_id", limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.text     "description", limit: 65535
   end
 
   add_index "recurrent_transactions", ["customer_id"], name: "index_recurrent_transactions_on_customer_id", using: :btree
